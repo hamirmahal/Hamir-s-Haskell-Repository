3090666b1f4aa695755d56b9dd701c9392b43e2a
Hamir Mahal <hamirmahal@gmail.com>
2022-04-19 16:49:27 -0700

    switch to multiline comment for easier copy/paste

--- main.hs
+++ main.hs
@@ -2,7 +2,10 @@ import Data.Char
 import Data.Function
 import Data.List
 
--- ghc -o main main.hs && ./main > main.hs.output.txt
+{-
+ghc -o main main.hs && ./main
+ghc -o main main.hs && ./main > main.hs.output.txt
+-}
 
 gitHubResponseString :: String
 gitHubResponseString = "device_code=f57537be76960aed42752846abd2602bc0bb49d5&expires_in=899&interval=5&user_code=B395-3C7A&verification_uri=https%3A%2F%2Fgithub.com%2Flogin%2Fdevice"
